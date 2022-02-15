97f0d1425581721791ce491eb4bbbda602f0b878
Meng87 <mroupurplax@gmail.com>
2022-02-14 22:22:46 -0500

    Added vidgrab script and README

---
+++ README.md
@@ -0,0 +1,66 @@
+README.md
+
+## Description
+This tool was developed to automate the process of downloading videos and transcripts, so as to facilitate the generation of large amounts of data for machine learning.
+
+## Dependencies and Installation Instructions
+
+* pytube
+    * pip install git+https://github.com/rmerzouki/pytube
+* xmltodict
+    * pip install xmltodict
+* Python 3.6 and above
+
+## Usage
+
+### Python Script
+To use VidGrab within your own Python script, you would first import the library and call the `get` function.
+~~~
+import vidgrab
+dict_out = vidgrab.get(url, video_save_path, caption_save_path)
+~~~
+
+### Shell Script
+To use this tool with the command line interface, you would use the following command:
+```
+python get.py -u <video url> -v <video save path> -c <caption save path>
+```
+There are also a number of optional flags you can use.
+| Flag               | Meaning     |
+| ------------------ | ----------- |
+| -w --write_caption | Write the output into a .json file       |
+| -d --debug         | Turn on debuging mode  |
+| -b --write_if_both | Only write the output into a .json file if both the video and captions are available |
+
+## Output
+The output will be a .json file containing the relevant information. If using the Python module, the function also returns the relevant information as a Python dictionary.
+
+The output follows the following format:
+~~~~
+{
+    'video_id': <string>,
+    'url': <string>,
+    'video_path': <string>,
+    'chunks': <list>
+    'intervals': <2D list>
+    'transcript_type': <string: "human" / "asr_youtube"
+}
+~~~~
+
+## Failure Cases
+Object returned will reflect error
+
+No English captions are available.
+{
+    'video_id': <string>,
+    'url': <string>,
+    'video_path': <string>,
+    'chunks': None,
+    'intervals': None,
+    'transcript_type': None
+}
+Video is not found.
+`{"error": "video_not_found"}`
+
+Unable to download the video.
+`{"error": "download_failed"}`
---
+++ vidgrab.py
@@ -0,0 +1,150 @@
+import sys, getopt, os
+import pytube
+import xmltodict, json
+
+def get(url, video_path, caption_path, write_caption=True, debug=False, write_if_both=True):
+   if (not os.path.isdir(video_path) or not os.path.isdir(caption_path)):
+      print("Error: Invalid File Path.")
+      return
+   
+   # Create YouTube object
+   try:
+      yt = pytube.YouTube(url)
+   except:
+      print("Error: Invalid URL.")
+      return
+   
+   filename = pytube.extract.video_id(url)
+
+   # Only download progressive streams
+   prog = yt.streams.filter(progressive=True)
+   bestVid = prog.get_highest_resolution()
+   if (bestVid == None):
+      if (debug): print("[DEBUG] Video not found.")
+      output = {"error": "video_not_found"}
+      if (write_caption):
+         json_data = json.dumps(output)
+         filepath = caption_path + "/" + filename
+         with open(filepath + ".json", "w+") as json_file:
+            json_file.write(json_data)
+      return output
+   else:
+      try:
+         bestVid.download(video_path, filename + ".mp4")
+      except:
+         if (debug): print("[DEBUG] Download Failed.")
+         output = {"error": "download_failed"}
+         if (write_caption):
+            json_data = json.dumps(output)
+            filepath = caption_path + "/" + filename
+            with open(filepath + ".json", "w+") as json_file:
+               json_file.write(json_data)
+         return output
+
+   # Get captions
+   transcript_type = None
+   if ('en' in yt.captions):
+      transcript_type = "human"
+      caption = yt.captions['en']
+   elif ('a.en' in yt.captions):
+      transcript_type = "asr_youtube"
+      caption = yt.captions['a.en']
+   else:
+      if (debug): print("[DEBUG] No English captions available.")
+      output = {
+         'video_id': filename,
+         'url': url,
+         'video_path': video_path + "/" + filename + ".mp4",
+         'chunks': None,
+         'intervals': None,
+         'transcript_type': None
+      }
+      if (write_caption and not write_if_both):
+         json_data = json.dumps(output)
+         filepath = caption_path + "/" + filename
+         with open(filepath + ".json", "w+") as json_file:
+            json_file.write(json_data)
+      return output
+      
+
+   if (caption != None):
+      filepath = caption_path + "/" + filename
+      
+      with open(filepath + ".xml", "w+") as caption_file:
+         caption_file.write(caption.xml_captions)
+         if (debug): print("[DEBUG] Saved captions as xml.")
+
+      with open(filepath + ".xml") as xml_file:
+         data_dict = xmltodict.parse(xml_file.read())
+      
+      # Convert to json
+      json_data = json.dumps(data_dict)
+      text_timings = data_dict["timedtext"]["body"]["p"]
+      chunks_list = [] # list of captions
+      intervals_list = [] # list of tuples of caption start, end times
+      if (transcript_type == "human"):
+         for d in text_timings:
+            start_time = d["@t"]
+            duration = d["@d"]
+            chunks_list.append(d["#text"])
+            intervals_list.append((start_time, str(int(start_time) + int(duration))))
+      else:
+         for d in text_timings:
+            if ("s" in d):
+               start_time = d["@t"]
+               duration = d["@d"]
+               chunk = []
+               if (isinstance(d["s"], list)):
+                  for t in d["s"]:
+                     chunk.append(t["#text"])
+                  chunk_str = " ".join(chunk)
+               elif (isinstance(d["s"], dict)):
+                  chunk_str = d["s"]["#text"]
+               chunks_list.append(chunk_str)
+               intervals_list.append((start_time, str(int(start_time) + int(duration))))
+            else: continue
+      output = {
+            'video_id': filename,
+            'url': url,
+            'video_path': video_path + "/" + filename + ".mp4",
+            'chunks': chunks_list,
+            'intervals': intervals_list,
+            'transcript_type': transcript_type
+         }
+      if (write_caption):
+         json_data = json.dumps(output)
+         filepath = caption_path + "/" + filename
+         with open(filepath + ".json", "w+") as json_file:
+            json_file.write(json_data)
+            if (debug): print("[DEBUG] Saved captions as json.")
+      # return output
+
+if __name__ == "__main__":
+   url, video_path, caption_path = None, None, None
+   write_caption, debug, write_if_both = True, False, True
+   try:
+      opts, args = getopt.getopt(sys.argv[1:],
+                                 "u:v:c:wdb",
+                                 ["url=","video_path=","caption_path=",
+                                  "write_caption=", "debug=",
+                                  "write_if_both=True="])
+   except getopt.GetoptError:
+      print("Usage: get.py -u <video url> -v <video save path> -c <caption save path>\n")
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt in ("-u", "--url"):
+         url = arg
+      elif opt in ("-v", "--video_path"):
+         video_path = arg
+      elif opt in ("-c", "--caption_path"):
+         caption_path = arg
+      elif opt in ("-w", "--write_caption"):
+         write_caption = True
+      elif opt in ("-d", "--debug"):
+         debug = True
+      elif opt in ("-b", "--write_if_both"):
+         write_if_both = True
+   if (url == None or video_path == None or caption_path == None): # check invalid
+      print("Usage: get.py -u <video url> -v <video save path> -c <caption save path>")
+      sys.exit(2)
+   get(url, video_path, caption_path, write_caption, debug, write_if_both)
